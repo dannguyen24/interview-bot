9cced3b206105423b5aedf4f3e367a2dfe29e829
Dan Nguyen <danuyennguyen2412005@gmail.com>
2025-10-04 09:20:22 -0400

    added backend

---
+++ backend/main.py
@@ -0,0 +1,23 @@
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+#parse job description
+
+#parse resume
+
+#generate questions based on job description and resume
+
+#feedback
